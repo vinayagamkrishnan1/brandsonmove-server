4f871bfdf7c025a4aff7baaeea58462299402ad5
Vinay K <vinayboi01@gmail.com>
2023-10-02 12:13:29 +0530

    correction - 1

--- src/app.ts
+++ src/app.ts
@@ -80,36 +80,3 @@ app.use(function(err: any, req: any, res: any, next: any) {
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
-
-// import express, { Application, Request, Response } from "express";
-// import bodyParser from "body-parser";
-// import cookieParser from "cookie-parser";
-// import compression from "compression";
-// import logger from "morgan";
-// import helmet from "helmet";
-// import cors from "cors";
-
-// import routes from "./routes/index";
-
-// const app: Application = express();
-
-// // Express middlewares
-// app.use(logger("dev"));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(express.json());
-// app.use(compression());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(helmet());
-// // app.use(express.static(path.join(__dirname, "public")));
-// app.use(cors());
-
-// // app.use("/", (req: Request, res: Response): void => {
-// //     res.send('Hello world!');
-// // });
-
-// // Routes
-// app.use('/', routes);
-
-// export default app;
