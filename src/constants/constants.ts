ab5225e0c8f0d01d79068c6f68f4132d971a5d92
Vinay K <vinayboi01@gmail.com>
2023-10-04 09:32:36 +0530

    correction - 2 --- email

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,9 +1,9 @@
 
 export const FCM_SERVER_KEY = "AAAAAbFpaFw:APA91bHdjIFoKbS9BR5SOifoppb3jblQfEux39zHPus2BF2H2VT-KZmHKGd2xQoUs6rFHTnlnvLDJ3yWrJIeJEza1RLq9_TQtlqC2cFXc8GXbsPzIVpVQlIXR1hOBBC41g-5IXe8ChC4";
 
-export const EMAIL = "vinayboi01@gmail.com";
-// App password
-export const PASSWORD = "tdmrtpblcssmeduy";
+// export const EMAIL = "vinayboi01@gmail.com";
+// // App password
+// export const PASSWORD = "tdmrtpblcssmeduy";
 
-// export const EMAIL = "rayapurv59@gmail.com";
-// export const PASSWORD = "piwhdkiqgsgvbmuv";
+export const EMAIL = "rayapurv59@gmail.com";
+export const PASSWORD = "piwhdkiqgsgvbmuv";
